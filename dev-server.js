10e0b2133ac8e4f84d6aa9776aad25fb70cc4e49
yolanda208 <799327507@qq.com>
2017-04-14 10:36:51 +0800

    添加 server 部分的 注释翻译 使用模块：express.logger 加入 响应压缩及 头高速缓存机制

--- dev-server.js
+++ dev-server.js
@@ -5,12 +5,14 @@
 "use strict";
 
 console.log("============================================================");
+//node端打印
 console.log(new Date().toISOString() + " - Starting");
 
 var util = require("util");
 
 /**
  * Returns true if the response should be compressed.
+ * 返回true  如果响应需要压缩？
  */
 function compressionFilter(req, res) {
     return (/json|text|javascript|font/).test(res.getHeader('Content-Type'));
@@ -18,22 +20,27 @@ function compressionFilter(req, res) {
 
 /**
  * Adds headers to a response to enable caching.
+ * 增加头（headers）用于启用 高速缓存 响应。
  */
 function cacheControl() {
-    return function(req, res, next) {
+    return function (req, res, next) {
         res.setHeader("Cache-Control", "public, max-age=300");
         return next();
     };
 }
 
+/**
+ * 记录日志函数 调用 express logger 模块
+ * @returns {*}
+ */
 function logger() {
-    express.logger.token("date", function() {
+    express.logger.token("date", function () {
         return new Date().toISOString();
     });
-    express.logger.token("response-all", function(req, res) {
+    express.logger.token("response-all", function (req, res) {
         return (res._header ? res._header : "").trim();
     });
-    express.logger.token("request-all", function(req, res) {
+    express.logger.token("request-all", function (req, res) {
         return util.inspect(req.headers);
     });
     return express.logger(
@@ -41,6 +48,7 @@ function logger() {
         '":user-agent" :referrer :req[cf-ray] :req[accept-encoding]\\n:request-all\\n\\n:response-all\\n');
 }
 
+//端口使用 node 的参数 2 node dev-server.js 8080
 var port = process.argv[2];
 var express = require("express");
 var app = express();
@@ -48,7 +56,10 @@ var app = express();
 app.use(cacheControl());
 app.use(express.compress({filter: compressionFilter}));
 app.use(logger());
+//将静态数据定位到 public 文件夹
 app.use(express.static("public"));
 
+//开启监听
 app.listen(port);
+//cmd 打印监听信息
 console.log("Listening on port " + port + "...");
